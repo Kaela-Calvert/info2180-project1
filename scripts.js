d9ae0b7974c3e2f7fe46192b8354dd28035e8a27
kaela <kaelacalvert14@gmail.com>
2023-11-02 15:45:03 -0500

    Edited Files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded",function() {
+    const email=document.getElementById("email");
+    let message=document.querySelector(".message");
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit",function(event){
+       
+        let userEmailAddress = email.value;
+
+        if(userEmailAddress.trim()===""){
+            message.innerHTML="Please enter a valid email address.";
+        }
+        else{
+            message.innerHTML="Thank you! Your email address" + " " +userEmailAddress+ " " + " has been added to our mailing list!";
+        }
+    });
+});
+    
